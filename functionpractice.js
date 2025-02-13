95bb9fb04c3904adc93d1d99114fc4b5b67fb66f
sumit9258 <sumitkumar925878@gmail.com>
2025-02-13 10:33:34 +0530

    function practice
    function practice

---
+++ functionpractice.js
@@ -0,0 +1,79 @@
+// function check(a) {
+//     if (a<2) return false
+    
+// for (let i = 2; i < a; i++) {
+//     if (a%i==0) {
+//         return false
+//     }
+// }
+//     return true
+// }
+
+
+// console.log(check(9))
+
+
+// let str="hello Javascript"
+// let str2=""
+// function revese(str) {
+    //     for (let i =str.length-1; i >=0; i--) {
+        
+    //         str2 +=str[i]
+    //     }
+    // console.log(str2);
+    
+    
+    // }
+    
+    // revese(str)
+    
+
+
+
+    // function first(a) {
+    //     if(a===1  || a===0) return 1
+    //     return a * first(a-1);
+    
+    // }
+        
+    
+    
+    // console.log(first(5));
+
+
+
+
+// let arr=[1,2,3,4,5]
+// let res=0
+
+// function sum(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         res+=arr[i]
+//     }
+// console.log(res);
+
+// }
+// sum(arr)
+
+
+
+// let str="mom"
+// res=""
+
+// function checkpailandrome(str) {
+//   for (let i = str.length-1; i >=0; i--) {
+// res+=str[i]
+// }  
+
+// if (str==res) {
+//     console.log("yes,it is Palindrome--->",str );
+    
+// }else{
+//     console.log("it is not a Palindrome--->",str);
+    
+// }
+
+// }
+
+// checkpailandrome(str)
+
